3dfafe13b52d52a4b3f54a3723fa0ac9d68269a9
trinhhoan00365-byte <trinhhoan00365@gmail.com>
2025-12-15 19:39:20 +0700

    Update data.js

--- js/data.js
+++ js/data.js
@@ -3,7 +3,7 @@ const MOVIES = [
     id: 1,
     title: "Phim Demo 1",
     duration: "07:49",
-    thumb: "https://picsum.photos/400/225?random=1",
+    thumb: "https://sf-static.upanhlaylink.com/img/image_202512152fadd20bc3f75baf0a2463d53d1314af.jpg",
     servers: [
       {
         name: "Server 1",
@@ -19,7 +19,7 @@ const MOVIES = [
     id: 2,
     title: "Phim Demo 2",
     duration: "05:30",
-    thumb: "https://picsum.photos/400/225?random=2",
+    thumb: "https://sf-static.upanhlaylink.com/img/image_20251215bc8ec1c9716583deadb4a9208d488328.jpg",
     servers: [
       {
         name: "Server 1",
